c5de6b80ef05cc2d1eab2ecc363d286be97b6234
ralem09 <ralem09@gmail.com>
2024-06-09 01:10:01 +0500

    Редактирование декор элементов

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,21 @@
 let header = document.querySelector('.header');
 let headerNavigation = document.querySelector('.header__navigation');
+let headerButton = document.querySelector('.header__button');
 
 
+header.classList.remove('header--nojs');
+headerNavigation.classList.add('header__navigation--closed')
+headerNavigation.classList.remove('header__navigation--open')
+headerButton.classList.add('header__button--open');
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+headerButton.addEventListener('click', function() {
+  if (headerNavigation.classList.contains('header__navigation--closed'))
+  {
+    headerButton.classList.remove('header__button--open')
+    headerButton.classList.add('header__button--closed')
+  } else (headerNavigation.classList.contains('header__navigation--open'))
+  {
+    headerButton.classList.remove('header__button--closed');
+    headerButton.classList.add('header__button--open');
   }
 });
